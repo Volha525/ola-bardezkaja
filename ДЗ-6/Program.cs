7993b75c40f6882fa33ca72af44ff43b152e0f31
Volha525 <63014275+Volha525@users.noreply.github.com>
2020-04-04 22:18:08 +0300

    Add files via upload

---
+++ ДЗ-6/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace StudentsCoffee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Stack:\n");
+
+            Stack<Student> students = new Stack<Student>();
+
+            students.Push(new Student() { Name = "Vitja Popov" });
+            students.Peek().DoHomework();
+            students.Push(new Student() { Name = "Boris Burda" });
+            students.Peek().DoHomework();
+            students.Push(new Student() { Name = "Tolja Hitrik" });
+            students.Peek().DoHomework();
+
+            while (students.Count > 0)
+            {
+                students.Pop().GetCoffee();
+            }
+
+            
+            Console.WriteLine("\nQueue:\n");
+
+            Queue<Student> studentsQueue = new Queue<Student>();
+
+            Student student;
+
+            studentsQueue.Enqueue(student = new Student () { Name = "Vitja Popov" });
+            student.DoHomework();
+            studentsQueue.Enqueue(student = new Student() { Name = "Boris Burda" });
+            student.DoHomework();
+            studentsQueue.Enqueue(student = new Student() { Name = "Tolja Hitrik" });
+            student.DoHomework();
+
+            while (studentsQueue.Count > 0)
+            {
+                studentsQueue.Dequeue().GetCoffee();
+            }
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
---
+++ ДЗ-6/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StudentsCoffee
+    {
+    internal class Student
+    {
+        public string Name;
+
+        public void DoHomework()
+        {
+            Console.WriteLine($"{Name} did homework");
+            
+        }
+
+        public void GetCoffee()
+        {
+            Console.WriteLine($"{Name} got coffee");
+        }
+    }
+}
